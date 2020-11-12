f088dac89274054f192e464a24e741147ded4a55
phillipad7 <phillip3517@gmail.com>
2020-11-11 23:25:28 -0800

    test GPIO.event_detected

--- pir_RPi_GPIO.py
+++ pir_RPi_GPIO.py
@@ -24,6 +24,7 @@ def callback_func(pin):
     t_diff = abs(pir1_last - pir2_last)
     t1 = start - pir1_last
     t2 = start - pir2_last
+
     if not t1 or not t2:
         print("t1:{}\nt2:{}\n".format(t1,t2))
     if t_diff < 1:
@@ -32,11 +33,30 @@ def callback_func(pin):
         print('longger than 1 second')
 
 # change GPIO.RISING to GPIO.FALIING if your PIRs are active low 
-GPIO.add_event_detect(12, GPIO.RISING, callback=callback_func)
-GPIO.add_event_detect(4, GPIO.RISING, callback=callback_func)
+# GPIO.add_event_detect(12, GPIO.RISING, callback=callback_func)
+# GPIO.add_event_detect(4, GPIO.RISING, callback=callback_func)
 
-def main():
-    while True:
-        print( "Not blocking! You're free to do other stuff here")
-        time.sleep(10)
+# def main():
+#     while True:
+#         print( "Not blocking! You're free to do other stuff here")
+#         time.sleep(10)
 
+
+# if __name__=="__main__":
+#     GPIO.add_event_detect(12, GPIO.RISING, callback=callback_func)
+#     GPIO.add_event_detect(4, GPIO.RISING, callback=callback_func)
+
+
+
+GPIO.add_event_detect(12, GPIO.RISING)  # add rising edge detection on a channel
+GPIO.add_event_detect(4,  GPIO.RISING)  #for both buttons
+
+start = time.time()
+while True:
+    if GPIO.event_detected(12):
+        print('PIR 1 detect')
+    if GPIO.event_detected(4):
+        print('PIR 2 detect')
+    if time.time() - start > 5:
+        print('Timeout')
+    time.sleep(0.1)
